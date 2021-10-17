07d5ada046e0f4738059ab348d7a65160e7132d1
denisklassen <klassendv@mail.ru>
2021-10-17 11:34:05 +0500

    1

--- Less_1/main.swift
+++ Less_1/main.swift
@@ -6,3 +6,25 @@
 //
 import Foundation
 
+// 1. Решить квадратное уравнение
+
+let a:Float = 1
+let b:Float = 2
+let c:Float = 3
+var x1:Float
+var x2:Float
+var d:Float
+var discriminant:Float
+
+d = b * b - (4 * a * c)
+
+if d >= 0{
+    discriminant = sqrt(d)
+    x1 = (-b + discriminant) / (2 * a)
+    x2 = (-b - (discriminant)) / (2 * a)
+    print(x1, x2)
+} else if(d < 0) {
+    d = ((4 * a * c) - pow(b,2)) / (2 * a)
+    print(d)
+}
+
